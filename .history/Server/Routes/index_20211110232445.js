519a01f945d73bad9c99d82b4a516a6f8ac77d7d
Jasmeet Kaur <jasmeetkaur0211@gmail.com>
2021-11-10 23:25:54 -0500

    updates to remove error in heroku

--- .history/Server/Routes/index_20211110232445.js
+++ .history/Server/Routes/index_20211110232445.js
@@ -8,27 +8,27 @@ const router = express_1.default.Router();
 exports.default = router;
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'home' });
+    res.render('index', { title: 'Home' });
 });
 /* GET home page. */
 router.get('/home', function (req, res, next) {
-    res.render('index', { title: 'home' });
+    res.render('index', { title: 'Home' });
 });
 /* GET about page. */
 router.get('/about', function (req, res, next) {
-    res.render('index', { title: 'about me' });
+    res.render('index', { title: 'About' });
 });
 /* GET projects page. */
 router.get('/projects', function (req, res, next) {
-    res.render('index', { title: 'projects' });
+    res.render('index', { title: 'Projects' });
 });
 /* GET services page. */
 router.get('/services', function (req, res, next) {
-    res.render('index', { title: 'services' });
+    res.render('index', { title: 'Services' });
 });
 /* GET contact page. */
 router.get('/contact', function (req, res, next) {
-    res.render('index', { title: 'contact me' });
+    res.render('index', { title: 'Contact' });
 });
 //module.exports = router;
 //# sourceMappingURL=index.js.map
